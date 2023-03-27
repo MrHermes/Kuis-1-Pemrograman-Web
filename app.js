a2863b8da7837382f5c0bf7fa517053d5bce22c7
Frederick Hidayat <90272678+MrHermes@users.noreply.github.com>
2023-03-27 15:56:57 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function submit_form(){
+    alert("Daily News each day, don't forget to come !");
+}
+
+function create(){
+    window.location="search.html";
+}
+
+function dark_mode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode"); 
+}
